c7d29dd0bbaa2b20715b705e2685b462fc4f27bd
avestoinn <avesto.inn@gmail.com>
2021-04-03 01:15:50 +0500

    Gunicorn support instead of broken uWSGI

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# We are on technical works at the moment
 @app.route('/')
 def index_page():
     return render_template("index.html")
